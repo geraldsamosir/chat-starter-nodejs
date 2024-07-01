18e57c5c20f40083170b35392aeda48b191c3d3a
geraldsamosir <gerald.unforgiven@gmail.com>
2024-07-01 18:24:59 +0700

    fix: database connection and layout register and login

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,16 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  // `${process.env.DATABASE_URL}` || "postgres://localhost:5432/messenger",
+  "messenger",
+  "admin",
+  "admin",
   {
+    host: 'localhost',
+    port: 5432,
     logging: false,
-  }
+    dialect: 'postgres'
+  },
 );
 
 module.exports = db;
